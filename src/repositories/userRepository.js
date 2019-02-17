16ba61e149ae8d21391d652684db719fb426d8ba
Fabio Romanini <fabioaromanini@gmail.com>
2019-02-17 01:04:19 -0300

    auth - promisifies updateToken method

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -15,17 +15,19 @@ module.exports.getUser = phoneNumber =>
 const tokenUpdateExpression =
   'SET #loginToken = :loginToken, updatedAt = :updatedAt';
 module.exports.updateToken = (phoneNumber, token) =>
-  dynamoDb.update({
-    TableName,
-    Key: {
-      phoneNumber,
-    },
-    UpdateExpression: tokenUpdateExpression,
-    ExpressionAttributeNames: {
-      '#loginToken': 'token',
-    },
-    ExpressionAttributeValues: {
-      ':loginToken': token,
-      ':updatedAt': new Date().toISOString(),
-    },
-  });
+  dynamoDb
+    .update({
+      TableName,
+      Key: {
+        phoneNumber,
+      },
+      UpdateExpression: tokenUpdateExpression,
+      ExpressionAttributeNames: {
+        '#loginToken': 'token',
+      },
+      ExpressionAttributeValues: {
+        ':loginToken': token,
+        ':updatedAt': new Date().toISOString(),
+      },
+    })
+    .promise();
