cdc1e527d7900f65f1177ec84ab39abffdbc8fda
Fabio Romanini <fabioaromanini@gmail.com>
2019-02-16 23:41:22 -0200

    auth - configs passport

---
+++ src/config/authentication.js
@@ -0,0 +1,51 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const userRepository = require('../repositories/userRepository');
+
+module.exports = app => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user.phoneNumber);
+  });
+
+  passport.deserializeUser(async (phoneNumber, callback) => {
+    try {
+      const { Item: user } = await userRepository.getUser(phoneNumber);
+      callback(null, user);
+    } catch (e) {
+      callback(e);
+    }
+  });
+
+  passport.use(
+    new Strategy(
+      {
+        usernameField: 'phoneNumber',
+        passwordField: 'token',
+      },
+      async (phoneNumber, token, cb) => {
+        const user = await userRepository.getUser(phoneNumber);
+
+        if (user === null) {
+          return cb(null, false, {
+            status: 403,
+            message: {
+              email: 'User not found',
+            },
+          });
+        } else if (user.token !== token) {
+          return cb(null, false, {
+            status: 403,
+            message: {
+              password: 'Invalid password',
+            },
+          });
+        }
+
+        return cb(null, user);
+      }
+    )
+  );
+};
