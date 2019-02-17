cb3f38a7f771116aed218b71db49836861c87cb3
Fabio Romanini <fabioaromanini@gmail.com>
2019-02-17 09:26:08 -0300

    refactors - cleans walletRoutes code

--- src/routes/walletRoutes.js
+++ src/routes/walletRoutes.js
@@ -1,33 +1,62 @@
 const paymentRepository = require('../repositories/zoopRepository');
 
-const HACK_PAY_ID =
-  process.env.HACK_PAY_ID || 'd9adc3c224d54b648f13fe6c431cf1fd';
+const { HACK_PAY_ID } = process.env;
 
 module.exports = app => {
   app.post('/wallet/transfer', async (req, res) => {
-    const user = req.user || {
-      id: '00594a5c10e240988a0e6e2842f067bf',
-      cashback: 0.005,
-      fee: 0.03,
-    };
-    const destiny = req.body.destiny || {
-      id: 'b61e502149194f7aa5550713b96d6d89',
-    };
-    const amount = parseInt(req.body.amount || '5000');
+    const user = req.user;
+    const { destiny, amount } = req.body;
+    const intAmount = parseInt(amount);
 
-    await paymentRepository.transactionP2P(amount, 'BUY', user.id, destiny.id);
+    // in order to achieve the fee payment, and cash-back, first we transfer
+    // money to the seller, then we transfer the cashback and fee from the seller to
+    // our platform, then finally we transfer the cashback to the user
     await paymentRepository.transactionP2P(
-      amount * (user.fee + user.cashback),
+      intAmount,
+      'BUY',
+      user.id,
+      destiny.id
+    );
+
+    await paymentRepository.transactionP2P(
+      intAmount * (user.fee + user.cashback),
       'FEE + CASHBACK',
       destiny.id,
       HACK_PAY_ID
     );
+
     await paymentRepository.transactionP2P(
-      amount * user.cashback,
+      intAmount * user.cashback,
       'CASHBACK',
       HACK_PAY_ID,
       user.id
     );
+
     res.send('');
+  });
+
+  app.post('/wallet/deposit', async (req, res) => {
+    const user = req.user;
+    const amount = parseInt(req.body.amount);
+
+    try {
+      await paymentRepository.preAuthCreditCard(
+        amount,
+        'CASH IN',
+        HACK_PAY_ID,
+        user.id
+      );
+
+      await paymentRepository.transactionP2P(
+        amount,
+        'CASH_IN',
+        HACK_PAY_ID,
+        user.id
+      );
+
+      res.send('ok');
+    } catch (e) {
+      res.status(403).send();
+    }
   });
 };
