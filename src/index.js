9baf96be1554ea4b140e59c3a97499a910361611
Fabio Romanini <fabioaromanini@gmail.com>
2019-02-20 17:07:59 -0300

    removes cardRoutes from app intialization

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 
 const configAuthentication = require('./config/authentication');
 const registerUserRoutes = require('./routes/userRoutes');
-const registerCardRoutes = require('./routes/cardRoutes');
 const registerWalletRoutes = require('./routes/walletRoutes');
 
 const app = express();
@@ -15,7 +14,6 @@ app.use(cors());
 configAuthentication(app);
 
 registerUserRoutes(app);
-registerCardRoutes(app);
 registerWalletRoutes(app);
 
 app.listen(8080, () => {
